60ba1985d62347099b599a7afd9e11f515ac6217
Franklin Webber <franklin.webber@gmail.com>
2012-05-18 18:28:48 -0400

    Section specs

--- lib/showoff/section.rb
+++ lib/showoff/section.rb
@@ -8,33 +8,37 @@ module ShowOff
   #
   class Section
 
-    def initialize
+    def initialize(params = {})
       @post_renderers = []
+      @sections = []
+      params.each {|k,v| send("#{k}=",v) if respond_to? "#{k}=" }
     end
-    
+
     # @return [String] the title of the section
     attr_accessor :title
-    
+
     # @return [Array<#slides>] returns an array of a Section objects or array
     #   of Slide objects.
     attr_reader :sections
-    
+
     #
     # Append slides or sections to this setion.
-    # 
-    # @param [Slide,Section,Array<Section>,Array<Slide>] content this any 
+    #
+    # @param [Slide,Section,Array<Section>,Array<Slide>] content this any
     #   slide or section that you want to add to this section.
     #
     def add_section(content)
-      (@sections ||= []) << Array(content).compact.flatten
-      @sections = @sections.compact.flatten
+      @sections = @sections + Array(content).compact.flatten.map do |content|
+        content.section = self
+        content
+      end
     end
 
     # @return [Array<#render>] returns a list of Renderers that will perform
     #   their renderering on the slides after the slides have all habe been
     #   rendered.
     attr_reader :post_renderers
-    
+
     #
     # @param [#render] renderer add a renderer, any object responding to
     #   #render, that will process the slides HTML content.
@@ -42,7 +46,7 @@ module ShowOff
     def add_post_renderer(renderer)
       @post_renderers << renderer
     end
-    
+
     # @return [String] HTML representation of the section
     def to_html
       sections.map do |section_or_slide|
---
+++ spec/showoff/section_spec.rb
@@ -0,0 +1,46 @@
+require_relative 'spec_helper'
+
+describe ShowOff::Section do
+
+  subject do
+    described_class.new :title => expected_title
+  end
+
+  let(:expected_title) { "Section Title" }
+
+  its(:title) { should eq expected_title }
+  its(:sections) { should be_empty }
+
+  describe "#add_section" do
+    before do
+      slide.stub(:to_a) { [ slide ] }
+      slide.stub(:section=)
+      section.stub(:to_a) { [ section ] }
+      section.stub(:section=)
+    end
+
+    let(:slide) { mock('Slide') }
+    let(:section) { mock('Section') }
+    let(:slides_and_sections) { [ slide, section ] }
+
+    context "when given a slide" do
+      it "should be appended to the sections" do
+        subject.add_section(slide)
+        subject.sections.should eq [ slide ]
+      end
+    end
+    context "when give a section" do
+      it "should be appended to the sections" do
+        subject.add_section(section)
+        subject.sections.should eq [ section ]
+      end
+    end
+    context "when given an array of slides and sections" do
+      it "should be appended to the sections" do
+        subject.add_section(slides_and_sections)
+        subject.sections.should eq slides_and_sections
+      end
+    end
+  end
+
+end
