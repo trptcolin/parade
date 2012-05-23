8197ed7556dfb67402e08ce7c773061787a2a85c
Franklin Webber <franklin.webber@gmail.com>
2012-05-23 14:11:51 -0700

    PDF generation uses the template generator

--- lib/showoff/features/pdf_presentation.rb
+++ lib/showoff/features/pdf_presentation.rb
@@ -6,15 +6,14 @@ module ShowOff
         
         # TODO: Find the presentation file and/or regenerate it every time
         
-        html_content = erb :onepage
-
+        template_options = {  'erb_template_file' => File.join(File.dirname(__FILE__), "..", "..", "views", "pdf.erb"),
+                              'slides' => slides }
+        
+        html_content = TemplateGenerator.new(template_options).render
+        
         # TODO the image references here are not full filepaths. creating issues
 
         kit = PDFKit.new(html_content,:page_size => 'Letter', :orientation => 'Landscape')
-
-        ['reset.css','showoff.css','theme/ui.all.css','ghf_marked.css','onepage.css','pdf.css'].each do |css_file|
-          kit.stylesheets << File.join(File.dirname(__FILE__),"..","..","public","css",css_file)
-        end
 
         send_file kit.to_file('presentation.pdf')
       end
