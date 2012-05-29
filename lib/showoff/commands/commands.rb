8b62430c9f429d453ecf11115a827485117a7a71
Franklin Webber <franklin.webber@gmail.com>
2012-05-29 00:44:42 -0700

    Refactored further into a new commands class

---
+++ lib/showoff/commands/commands.rb
@@ -0,0 +1,57 @@
+require_relative 'static_html'
+require_relative 'static_pdf'
+require_relative 'unknown'
+require_relative 'generate_presentation'
+require_relative 'generate_outline'
+require_relative 'generate_rackup'
+
+module ShowOff
+  module Commands
+
+    class Commands
+
+      def self.commands_with_unknown_default
+        command_hash = {}
+        command_hash.default = Unknown.new
+        command_hash
+      end
+
+      def self.static(name,command=nil)
+        @statics ||= commands_with_unknown_default
+
+        if command
+          @statics[name] = command
+        else
+          @statics[name]
+        end
+      end
+
+      def self.statics
+        @statics.values
+      end
+
+      static "html", StaticHtml.new
+      static "pdf", StaticPdf.new
+
+
+      def self.generator(name,command=nil)
+        @generators ||= commands_with_unknown_default
+        if command
+          @generators[name] = command
+        else
+          @generators[name]
+        end
+      end
+
+      def self.generators
+        @generators.values
+      end
+
+      generator "presentation", GeneratePresentation.new
+      generator "outline", GenerateOutline.new
+      generator "rackup", GenerateRackup.new
+
+    end
+
+  end
+end
--- lib/showoff/utilities.rb
+++ lib/showoff/utilities.rb
@@ -1,59 +1,31 @@
 require_relative 'helpers/template_generator'
 require_relative 'renderers/inline_images'
-require_relative 'commands/static_html'
-require_relative 'commands/static_pdf'
-require_relative 'commands/unknown'
-require_relative 'commands/generate_presentation'
-require_relative 'commands/generate_outline'
-require_relative 'commands/generate_rackup'
+require_relative 'commands/commands'
 
 module ShowOff
 
   module Utilities
     extend self
-
-    def static_commands
-      [ ShowOff::Commands::StaticHtml.new, ShowOff::Commands::StaticPdf.new ]
-    end
-
-    def static_command(name)
-      all_commands = static_commands.map {|command| [ command.name, command ] }.flatten
-      command_hash = Hash[*all_commands]
-      command_hash.default = ShowOff::Commands::Unknown.new
-      command_hash[name]
-    end
-
+    
     def statics
-      all_commands = static_commands.map {|command| [command.name, command.description] }.flatten
-      Hash[*all_commands]
-    end
-
-    def static(output_type,options)
-      command = static_command(output_type)
-      command.generate(options)
-    end
-
-    def generate_commands
-      [ ShowOff::Commands::GeneratePresentation.new,
-        ShowOff::Commands::GenerateOutline.new,
-        ShowOff::Commands::GenerateRackup.new ]
-    end
-
-    def generators
-      all_commands = generate_commands.map {|command| [command.name, command.description] }.flatten
+      all_commands = ShowOff::Commands::Commands.statics.map {|command| [command.name, command.description] }.flatten
       Hash[*all_commands]
     end
     
-    def generator(name)
-      all_commands = generate_commands.map {|command| [ command.name, command ] }.flatten
-      command_hash = Hash[*all_commands]
-      command_hash.default = ShowOff::Commands::Unknown.new
-      command_hash[name]
+    def static(output_type,options)
+      puts "Generating #{output_type} with #{options}"
+      command = ShowOff::Commands::Commands.static(output_type)
+      command.generate(options)
+    end
+
+    def generators
+      all_commands = ShowOff::Commands::Commands.generators.map {|command| [command.name, command.description] }.flatten
+      Hash[*all_commands]
     end
     
     def generate(asset_name,options)
       puts "Generating #{asset_name} with #{options}"
-      command = generator(asset_name)
+      command = ShowOff::Commands::Commands.generator(asset_name)
       command.generate(options)
     end
   end
